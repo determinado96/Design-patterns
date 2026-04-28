54ef7e60f7128753c78c2c3268588c6c874c330e
= <rodirodrigorodrigues@gmail.com>
2026-04-28 15:17:31 -0300

    refact: troca o exemplo bridge de js para ts

--- src/structural/bridge/ComBridge.ts
+++ src/structural/bridge/ComBridge.ts
@@ -1,47 +1,49 @@
 // Implementação: como eu faço isso (TVs)
-class TV {
-  ligar() {}
+abstract class TV {
+  abstract ligar(): void;
 }
 
 class SamsungTV extends TV {
-  ligar() {
+  override ligar(): void {
     console.log("Ligando TV Samsung");
   }
 }
 
 class LgTV extends TV {
-  ligar() {
+  override ligar(): void {
     console.log("Ligando TV LG");
   }
 }
 
 // Abstração: o que eu quero fazer (controle remoto)
 class ControleRemoto {
-  constructor(tv) {
+  protected tv: TV;
+
+  constructor(tv: TV) {
     // Composição: o controle remoto tem uma TV
     this.tv = tv;
   }
 
-  ligar() {
+  ligar(): void {
     this.tv.ligar();
   }
 }
 
 class ControleAvancado extends ControleRemoto {
-  netflix() {
+  netflix(): void {
     console.log("Abrindo Netflix");
   }
 }
 
-const samsung = new SamsungTV();
-const lg = new LgTV();
+const samsung: TV = new SamsungTV();
+const lg: TV = new LgTV();
 
 const controle1 = new ControleRemoto(samsung);
-controle1.ligar(); 
+controle1.ligar();
 // Ligando TV Samsung
 
 const controle2 = new ControleAvancado(lg);
-controle2.ligar(); 
+controle2.ligar();
 // Ligando TV LG
 
 controle2.netflix();
---
+++ src/structural/bridge/SemBridge.ts
@@ -0,0 +1,35 @@
+﻿interface TVControle {
+  ligar(): void;
+}
+
+class ControleSamsung implements TVControle {
+  ligar(): void {
+    console.log("Ligando TV Samsung");
+  }
+}
+
+class ControleLg implements TVControle {
+  ligar(): void {
+    console.log("Ligando TV LG");
+  }
+}
+
+class ControleAvancadoSamsung implements TVControle {
+  ligar(): void {
+    console.log("Ligando TV Samsung");
+  }
+
+  netflix(): void {
+    console.log("Abrindo Netflix na Samsung");
+  }
+}
+
+class ControleAvancadoLg implements TVControle {
+  ligar(): void {
+    console.log("Ligando TV LG");
+  }
+
+  netflix(): void {
+    console.log("Abrindo Netflix na LG");
+  }
+}
